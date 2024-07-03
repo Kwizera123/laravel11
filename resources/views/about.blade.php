dcb2527236d97732dea3586829e2c00308ba0783
Kwizera123 <kwizerasinapy49@gmail.com>
2024-07-03 23:06:26 +0200

    second commit

--- resources/views/about.blade.php
+++
@@ -1,7 +0,0 @@
-
-<x-layout>
-<x-slot:heading>
-    About page
-  </x-slot:heading>
-<h1>Hello form the About page!</h1>
-</x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,7 @@
   <x-slot:heading>
     Home page
   </x-slot:heading>
-<h1>Hello form the Home page!</h1>
+<h1>Hello form the Home page..</h1>
+
+
 </x-layout>
